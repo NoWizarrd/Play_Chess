bd0c51618f93ac6ad28d946d2e0d4b6a684cb60d
Wizard <pnev.leha@mail.ru>
2023-04-13 15:32:56 +0700

    Профиль

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:play_chess/pages/StartPage.dart';
 
 class ProfilePage extends StatefulWidget{
   const ProfilePage({Key? key}) : super(key: key);
@@ -14,6 +15,10 @@ class _ProfilePageState extends State<ProfilePage> {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
+    void _goToMainScreen(BuildContext context) {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => StartPage()));
+    }
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(36, 36, 36, 1),
       body: SafeArea(
@@ -21,8 +26,12 @@ class _ProfilePageState extends State<ProfilePage> {
           padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
           margin: EdgeInsets.fromLTRB(screenWidth / 40, screenHeight / 30, screenWidth / 40, screenHeight / 30),
           decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color.fromRGBO(94, 94, 94, 1.0), Color.fromRGBO(48, 48, 48, 1.0),],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
               borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Color.fromRGBO(48, 48, 48, 1.0),
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
@@ -107,7 +116,7 @@ class _ProfilePageState extends State<ProfilePage> {
 
                   alignment: Alignment.bottomCenter,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () => _goToMainScreen(context),
                     child: Text(
                         'На главный экран',
                       style: TextStyle(
