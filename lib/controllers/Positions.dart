361688895436cb16e5a360cc8e3837acf7aecf70
Maks <zasuherok2002@gmail.com>
2023-05-11 21:08:59 +0700

    -Добавил миллион строк коода логики правил шахмат(не добавил возможность рубить  и пешке меняться на ферзя) -Добавил всё на поле -Поменял местами логику

--- lib/controllers/Positions.dart
+++ lib/controllers/Positions.dart
@@ -4,13 +4,23 @@ import 'package:play_chess/models/piece/Pieces.dart';
 
 class Position
 {
-  List <Piece> pieces = [King('Белый Король', 5, Colors.white)];
+  List <Piece> pieces = [Rook('Чёрная ладья', 8, Colors.black), Knight('Чёрный конь', 7, Colors.black),Bishop('Чёрный слон', 6, Colors.black),
+    King('Чёрный Король', 5, Colors.black), Queen('Чёрный Ферзь', 4, Colors.black),Knight('Чёрный конь', 2, Colors.black),
+    Bishop('Чёрный слон', 3, Colors.black), Rook('Чёрная ладья', 1, Colors.black), Pawn('Белая пешка', 9, Colors.black),
+    Pawn('Белая пешка', 10, Colors.black), Pawn('Белая пешка', 11, Colors.black), Pawn('Белая пешка', 12, Colors.black), Pawn('Белая пешка', 13, Colors.black),
+    Pawn('Чёрная пешка', 14, Colors.black), Pawn('Белая пешка', 15, Colors.black), Pawn('Белая пешка', 16, Colors.black),
+    Rook('Белая ладья', 64, Colors.white), Knight('Белый конь', 63, Colors.white),Bishop('Белый слон', 62, Colors.white),
+    King('Белый Король', 61, Colors.white), Queen('Белый Ферзь', 60, Colors.white),Knight('Белый конь', 58, Colors.white),
+    Bishop('Белый слон', 59, Colors.white), Rook('Белая ладья', 57, Colors.white), Pawn('Белая пешка', 49, Colors.white),
+    Pawn('Белая пешка', 56, Colors.white), Pawn('Белая пешка', 55, Colors.white), Pawn('Белая пешка', 54, Colors.white), Pawn('Белая пешка', 53, Colors.white),
+    Pawn('Белая пешка', 52, Colors.white), Pawn('Белая пешка', 51, Colors.white), Pawn('Белая пешка', 50, Colors.white),
+  ];
   List ruleFigure(int key)
   {
     List list = [];
     for (Piece element in pieces) {
       if (key == element.position){
-        list = element.rule();
+        list = element.rule(pieces);
       }
     }
     return list;
--- lib/models/piece/Pieces.dart
+++ lib/models/piece/Pieces.dart
@@ -6,7 +6,7 @@ abstract class Piece{
   abstract String image;
   abstract Color color;
   Piece();
-  List rule();
+  List rule(List listPieces);
 }
 
 
@@ -21,8 +21,10 @@ class King extends Piece{
   Color color = Colors.black;
   King(this.name, this.position, this.color);
   @override
-  List rule() {
+
+  List rule(List listPieces) {
     List keys = [];
+    List temporaryKeys = [];
     if ((position - 1) % 8 != 0)
     {
       keys.add(position - 1);
@@ -39,6 +41,36 @@ class King extends Piece{
     {
       keys.add(position + 8);
     }
+    if ((position < 57) && ((position - 1) % 8 != 0))
+    {
+      keys.add(position + 7);
+    }
+    if ((position < 57) && (position % 8 != 0))
+    {
+      keys.add(position + 9);
+    }
+    if ((position > 8) && (position % 8 != 0))
+    {
+      keys.add(position - 7);
+    }
+    if ((position > 8) && ((position - 1) % 8 != 0))
+    {
+      keys.add(position - 9);
+    }
+    for (int key in keys)
+    {
+      for (Piece piece in listPieces)
+      {
+        if ((piece.position == key) && (piece.color == color))
+        {
+          temporaryKeys.add(key);
+        }
+      }
+    }
+    for (int key in temporaryKeys)
+    {
+      keys.remove(key);
+    }
     return keys;
   }
 }
@@ -54,21 +86,598 @@ class Queen extends Piece
   @override
   Color color = Colors.black;
   Queen(this.name, this.position, this.color);
+
   @override
-  List rule()
+  List rule(List listPieces)
   {
     List keys = [];
-    int temporaryPosition = position;
-    while (temporaryPosition > 8)
+
+    int temporaryPositionVertical = position;
+    int temporaryPositionHorizontal = position;
+    bool notFound = false;
+    int numb = 0;
+    while (temporaryPositionVertical > 8)
+      {
+        temporaryPositionVertical -= 8;
+        if ((temporaryPositionHorizontal  - 1) % 8 != 0)
+          {
+            temporaryPositionHorizontal--;
+            numb++;
+            for (Piece piece in listPieces)
+            {
+              if (piece.position == temporaryPositionVertical - numb)
+              {
+                if (piece.color != color)
+                {
+                  keys.add(temporaryPositionVertical - numb);
+                }
+                notFound = true;
+              }
+            }
+            if (notFound)
+            {
+              break;
+            }
+            keys.add(temporaryPositionVertical - numb);
+          }
+      }
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    numb = 0;
+
+    while (temporaryPositionVertical < 57)
     {
-      temporaryPosition -= 8;
-      keys.add(temporaryPosition);
+      temporaryPositionVertical += 8;
+      if ((temporaryPositionHorizontal  - 1) % 8 != 0)
+      {
+        temporaryPositionHorizontal--;
+        numb++;
+        for (Piece piece in listPieces)
+        {
+          if (piece.position == temporaryPositionVertical - numb)
+          {
+            if (piece.color != color)
+            {
+              keys.add(temporaryPositionVertical - numb);
+            }
+            notFound = true;
+          }
+        }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionVertical - numb);
+      }
     }
-    while (temporaryPosition > 8)
+
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    numb = 0;
+    while (temporaryPositionVertical > 8)
     {
-      temporaryPosition -= 8;
-      keys.add(temporaryPosition);
+      temporaryPositionVertical -= 8;
+      if (temporaryPositionHorizontal % 8 != 0)
+      {
+        temporaryPositionHorizontal++;
+        numb++;
+        for (Piece piece in listPieces)
+        {
+          if (piece.position == temporaryPositionVertical + numb)
+          {
+            if (piece.color != color)
+            {
+              keys.add(temporaryPositionVertical + numb);
+            }
+            notFound = true;
+          }
+        }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionVertical + numb);
+      }
     }
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    numb = 0;
+    while (temporaryPositionVertical < 57)
+    {
+      temporaryPositionVertical += 8;
+      if (temporaryPositionHorizontal % 8 != 0)
+      {
+        temporaryPositionHorizontal++;
+        numb++;
+        for (Piece piece in listPieces)
+        {
+          if (piece.position == temporaryPositionVertical + numb)
+          {
+            if (piece.color != color)
+            {
+              keys.add(temporaryPositionVertical + numb);
+            }
+            notFound = true;
+          }
+        }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionVertical + numb);
+      }
+    }
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    while (temporaryPositionVertical > 8)
+    {
+      temporaryPositionVertical -= 8;
+      for (Piece piece in listPieces)
+      {
+        if (piece.position == temporaryPositionVertical)
+        {
+          if (piece.color != color)
+          {
+            keys.add(temporaryPositionVertical);
+          }
+          notFound = true;
+        }
+      }
+      if (notFound)
+      {
+        break;
+      }
+      keys.add(temporaryPositionVertical);
+    }
+
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    while (temporaryPositionVertical < 57)
+    {
+      temporaryPositionVertical += 8;
+      for (Piece piece in listPieces)
+      {
+        if (piece.position == temporaryPositionVertical)
+        {
+          if (piece.color != color)
+          {
+            keys.add(temporaryPositionVertical);
+          }
+          notFound = true;
+        }
+      }
+      if (notFound)
+      {
+        break;
+      }
+      keys.add(temporaryPositionVertical);
+    }
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    while (temporaryPositionHorizontal % 8 != 0)
+      {
+        temporaryPositionHorizontal++;
+        for (Piece piece in listPieces)
+          {
+            if (piece.position == temporaryPositionHorizontal)
+              {
+                if (piece.color != color)
+                  {
+                    keys.add(temporaryPositionHorizontal);
+                  }
+                notFound = true;
+              }
+          }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionHorizontal);
+
+      }
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    while ((temporaryPositionHorizontal - 1)  % 8 != 0)
+    {
+      temporaryPositionHorizontal--;
+      for (Piece piece in listPieces)
+      {
+        if (piece.position == temporaryPositionHorizontal)
+        {
+          if (piece.color != color)
+          {
+            keys.add(temporaryPositionHorizontal);
+          }
+          notFound = true;
+        }
+      }
+      if (notFound)
+      {
+        break;
+      }
+      keys.add(temporaryPositionHorizontal);
+    }
+    return keys;
+  }
+}
+
+class Knight extends Piece
+{
+  @override
+  String name = '';
+  @override
+  int position = 1;
+  @override
+  String image = 'images/Конь.svg';
+  @override
+  Color color = Colors.black;
+  Knight(this.name, this.position, this.color);
+  @override
+  List rule(List listPieces)
+  {
+    List keys = [];
+    List temporaryKeys = [];
+    if ((position + 1) % 8 != 0)
+      {
+        keys.add(position + 10);
+        keys.add(position - 6);
+      }
+    if (position % 8 != 0)
+    {
+      keys.add(position + 17);
+      keys.add(position - 15);
+    }
+    if ((position - 2) % 8 != 0)
+      {
+        keys.add(position + 6);
+        keys.add(position - 10);
+      }
+    if ((position - 1) % 8 != 0)
+    {
+      keys.add(position + 15);
+      keys.add(position - 17);
+    }
+    for (int key in keys)
+    {
+      for (Piece piece in listPieces)
+      {
+        if ((piece.position == key) && (piece.color == color))
+        {
+          temporaryKeys.add(key);
+        }
+      }
+    }
+    for (int key in temporaryKeys)
+      {
+        keys.remove(key);
+      }
+    keys.removeWhere((element) => element < 1);
+    keys.removeWhere((element) => element > 64);
+    return keys;
+  }
+}
+
+
+class Bishop extends Piece
+{
+  @override
+  String name = '';
+  @override
+  int position = 1;
+  @override
+  String image = 'images/Слон.svg';
+  @override
+  Color color = Colors.black;
+  Bishop(this.name, this.position, this.color);
+  @override
+  @override
+  List rule(List listPieces)
+  {
+    List keys = [];
+
+    int temporaryPositionVertical = position;
+    int temporaryPositionHorizontal = position;
+    bool notFound = false;
+    int numb = 0;
+    while (temporaryPositionVertical > 8)
+    {
+      temporaryPositionVertical -= 8;
+      if ((temporaryPositionHorizontal  - 1) % 8 != 0)
+      {
+        temporaryPositionHorizontal--;
+        numb++;
+        for (Piece piece in listPieces)
+        {
+          if (piece.position == temporaryPositionVertical - numb)
+          {
+            if (piece.color != color)
+            {
+              keys.add(temporaryPositionVertical - numb);
+            }
+            notFound = true;
+          }
+        }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionVertical - numb);
+      }
+    }
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    numb = 0;
+
+    while (temporaryPositionVertical < 57)
+    {
+      temporaryPositionVertical += 8;
+      if ((temporaryPositionHorizontal  - 1) % 8 != 0)
+      {
+        temporaryPositionHorizontal--;
+        numb++;
+        for (Piece piece in listPieces)
+        {
+          if (piece.position == temporaryPositionVertical - numb)
+          {
+            if (piece.color != color)
+            {
+              keys.add(temporaryPositionVertical - numb);
+            }
+            notFound = true;
+          }
+        }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionVertical - numb);
+      }
+    }
+
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    numb = 0;
+    while (temporaryPositionVertical > 8)
+    {
+      temporaryPositionVertical -= 8;
+      if (temporaryPositionHorizontal % 8 != 0)
+      {
+        temporaryPositionHorizontal++;
+        numb++;
+        for (Piece piece in listPieces)
+        {
+          if (piece.position == temporaryPositionVertical + numb)
+          {
+            if (piece.color != color)
+            {
+              keys.add(temporaryPositionVertical + numb);
+            }
+            notFound = true;
+          }
+        }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionVertical + numb);
+      }
+    }
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    numb = 0;
+    while (temporaryPositionVertical < 57)
+    {
+      temporaryPositionVertical += 8;
+      if (temporaryPositionHorizontal % 8 != 0)
+      {
+        temporaryPositionHorizontal++;
+        numb++;
+        for (Piece piece in listPieces)
+        {
+          if (piece.position == temporaryPositionVertical + numb)
+          {
+            if (piece.color != color)
+            {
+              keys.add(temporaryPositionVertical + numb);
+            }
+            notFound = true;
+          }
+        }
+        if (notFound)
+        {
+          break;
+        }
+        keys.add(temporaryPositionVertical + numb);
+      }
+    }
+    return keys;
+  }
+}
+
+class Rook extends Piece
+{
+  @override
+  String name = '';
+  @override
+  int position = 1;
+  @override
+  String image = 'images/Ладья.svg';
+  @override
+  Color color = Colors.black;
+  Rook(this.name, this.position, this.color);
+  @override
+  List rule(List listPieces)
+  {
+    List keys = [];
+    int temporaryPositionVertical = position;
+    int temporaryPositionHorizontal = position;
+    bool notFound = false;
+    while (temporaryPositionVertical > 8)
+    {
+      temporaryPositionVertical -= 8;
+      for (Piece piece in listPieces)
+      {
+        if (piece.position == temporaryPositionVertical)
+        {
+          if (piece.color != color)
+          {
+            keys.add(temporaryPositionVertical);
+          }
+          notFound = true;
+        }
+      }
+      if (notFound)
+      {
+        break;
+      }
+      keys.add(temporaryPositionVertical);
+    }
+
+
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    while (temporaryPositionVertical < 57)
+    {
+      temporaryPositionVertical += 8;
+      for (Piece piece in listPieces)
+      {
+        if (piece.position == temporaryPositionVertical)
+        {
+          if (piece.color != color)
+          {
+            keys.add(temporaryPositionVertical);
+          }
+          notFound = true;
+        }
+      }
+      if (notFound)
+      {
+        break;
+      }
+      keys.add(temporaryPositionVertical);
+    }
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    while (temporaryPositionHorizontal % 8 != 0)
+    {
+      temporaryPositionHorizontal++;
+      for (Piece piece in listPieces)
+      {
+        if (piece.position == temporaryPositionHorizontal)
+        {
+          if (piece.color != color)
+          {
+            keys.add(temporaryPositionHorizontal);
+          }
+          notFound = true;
+        }
+      }
+      if (notFound)
+      {
+        break;
+      }
+      keys.add(temporaryPositionHorizontal);
+
+    }
+    temporaryPositionVertical = position;
+    temporaryPositionHorizontal = position;
+    notFound = false;
+    while ((temporaryPositionHorizontal - 1)  % 8 != 0)
+    {
+      temporaryPositionHorizontal--;
+      for (Piece piece in listPieces)
+      {
+        if (piece.position == temporaryPositionHorizontal)
+        {
+          if (piece.color != color)
+          {
+            keys.add(temporaryPositionHorizontal);
+          }
+          notFound = true;
+        }
+      }
+      if (notFound)
+      {
+        break;
+      }
+      keys.add(temporaryPositionHorizontal);
+    }
+    return keys;
+  }
+}
+
+class Pawn extends Piece
+{
+  @override
+  String name = '';
+  @override
+  int position = 1;
+  @override
+  String image = 'images/Пешка.svg';
+  @override
+  Color color = Colors.black;
+  Pawn(this.name, this.position, this.color);
+  @override
+  rule(List listPieces)
+  {
+    List keys = [];
+    int coefficient;
+    if (color == Colors.white)
+      {
+        coefficient = -1;
+      }
+    else
+      {
+        coefficient = 1;
+      }
+    keys.add(position + 8 * coefficient);
+    keys.add(position + 16 * coefficient);
+    keys.removeWhere((element) => element < 1);
+    keys.removeWhere((element) => element > 64);
+    for (Piece piece in listPieces)
+      {
+        if (piece.position == position + 7 * coefficient)
+        {
+          if (piece.color != color)
+          {
+            keys.add(piece.position);
+          }
+        }
+        if (piece.position == position + 9 * coefficient)
+        {
+          if (piece.color != color)
+          {
+            keys.add(piece.position);
+          }
+        }
+        if (piece.position == position + 8 * coefficient)
+        {
+          if (piece.color != color)
+          {
+            keys.removeWhere((element) => element == piece.position);
+          }
+        }
+      }
     return keys;
   }
 }
